77eb5c0701129e05b4f963a300dde409272ec5f5
Kevin <stradtkt22@gmail.com>
2019-09-01 08:30:22 -0400

    Finished the french app

--- app/src/main/java/com/stradtman/basicphrases/MainActivity.java
+++ app/src/main/java/com/stradtman/basicphrases/MainActivity.java
@@ -2,13 +2,27 @@ package com.stradtman.basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void buttonTapped(View view) {
+        int id = view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.stradtman.basicphrases");
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+//        Log.i(TAG, "buttonTapped: true" + Integer.toString(view.getId()));
+//        Log.i(TAG, "buttonTapped: ourId: " + ourId);
     }
 }
